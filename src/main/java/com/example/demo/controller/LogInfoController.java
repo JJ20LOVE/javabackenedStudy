9a4e640e84a8dc024f10dbc7a141729c1d610812
miaowu <2368691796@qq.com>
2025-12-15 14:23:21 +0800

    增加了基于jwt令牌的登录功能

--- src/main/java/com/example/demo/controller/LogInfoController.java
+++ src/main/java/com/example/demo/controller/LogInfoController.java
@@ -8,8 +8,10 @@ import com.example.demo.pojo.Result;
 import com.example.demo.service.EmpService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,8 +25,10 @@ public class LogInfoController {
     public Result login(@RequestBody Emp emp){
         log.info("登录");
         LoginInfo login =empService.login(emp);
-        if(login== null)
-            return Result.error("登录失败");
+        if(login== null) {
+            // 登录失败时返回401状态码
+            return Result.error("用户名或密码错误");
+        }
         return Result.success(login);
     }
 }
